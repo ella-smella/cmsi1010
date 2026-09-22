ee4b438c496cab1e86a750727676ff5c1da24cd2
El Dumbrique <elladumbrique@gmail.com>
2026-09-21 17:00:38 -0700

    use break the exit the petting zoo dialog loop

--- lab04/zoo.py
+++ lab04/zoo.py
@@ -24,8 +24,7 @@ def pet_animal(animal):
 print("Welcome to the Petting Zoo!")
 print("Type 'help' to get a list of all the things you can do")
 print()
-keep_going = True
-while keep_going:
+while True:
     response = input("What would you like to do? ").strip().lower()
     if response == "help":
         show_help()
@@ -36,6 +35,6 @@ while keep_going:
         pet_animal(animal)
     elif response == "bye":
         print("Goodbye!")
-        keep_going = False
+        break
     else:
         print("Sorry, the zoo is still under construction 🚧")
